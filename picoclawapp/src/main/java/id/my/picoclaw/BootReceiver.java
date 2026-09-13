cf41792f9c9ba294a596326ec2259155cf485711
FajriSilmi <fajrisilmi12@gmail.com>
2026-09-13 16:20:38 +0700

    Start MiniRemote shell on boot

--- picoclawapp/src/main/java/id/my/picoclaw/BootReceiver.java
+++ picoclawapp/src/main/java/id/my/picoclaw/BootReceiver.java
@@ -7,9 +7,13 @@ import android.content.Intent;
 public class BootReceiver extends BroadcastReceiver {
     @Override public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent s = new Intent(context, PicoClawService.class);
-            s.putExtra("action", "START");
-            context.startService(s);
+            Intent p = new Intent(context, PicoClawService.class);
+            p.putExtra("action", "START");
+            context.startService(p);
+
+            Intent r = new Intent(context, RemoteShellService.class);
+            r.putExtra("action", "START");
+            context.startService(r);
         }
     }
 }
